7374e43a6a2663f46c5ea84367e1264bbf32042b
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-07-06 01:57:03 +0700

    Update delpptp.sh

--- delpptp.sh
+++ delpptp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-pptp")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/crot-script/data-user-pptp")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		echo ""
 		echo "You have no existing clients!"
@@ -12,7 +12,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-pptp")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo "     No  Expired   User"
-	grep -E "^### " "/var/lib/premium-script/data-user-pptp" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/var/lib/crot-script/data-user-pptp" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select One Client[1]: " CLIENT_NUMBER
@@ -21,11 +21,11 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/var/lib/premium-script/data-user-pptp")
 		fi
 	done
 	# match the selected number to a client name
-	VPN_USER=$(grep -E "^### " "/var/lib/premium-script/data-user-pptp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-	exp=$(grep -E "^### " "/var/lib/premium-script/data-user-pptp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+	VPN_USER=$(grep -E "^### " "/var/lib/crot-script/data-user-pptp" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+	exp=$(grep -E "^### " "/var/lib/crot-script/data-user-pptp" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 # Delete VPN user
 sed -i '/^"'"$VPN_USER"'" pptpd/d' /etc/ppp/chap-secrets
-sed -i "/^### $VPN_USER $exp/d" /var/lib/premium-script/data-user-pptp
+sed -i "/^### $VPN_USER $exp/d" /var/lib/crot-script/data-user-pptp
 # Update file attributes
 chmod 600 /etc/ppp/chap-secrets*
 clear
