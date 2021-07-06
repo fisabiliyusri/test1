50f0671a31636753cce042499c3b8ea3a33e9913
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-07-07 06:44:43 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -18,7 +18,7 @@ cp -r /etc/wireguard backup/wireguard
 cp /etc/ppp/chap-secrets backup/chap-secrets
 cp /etc/ipsec.d/passwd backup/passwd1
 cp /etc/shadowsocks-libev/akun.conf backup/ss.conf
-cp -r /var/lib/premium-script/ backup/premium-script
+cp -r /var/lib/crot-script/ backup/crot-script
 cp -r /home/sstp backup/sstp
 cp -r /etc/v2ray backup/v2ray
 cp -r /etc/trojan backup/trojan
