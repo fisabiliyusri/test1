39102e2ec24864a38fd73dc2637e2d95e8ae8bf0
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-07-07 12:53:49 +0700

    Update ws-ovpn.py

--- websocket-python/ws-ovpn.py
+++ websocket-python/ws-ovpn.py
@@ -14,7 +14,7 @@ BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:1197'
 RESPONSE = 'HTTP/1.1 101 SL_OpenVPN_YNTKTS\r\n\r\n'
-#RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
+##RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 class Server(threading.Thread):
     def __init__(self, host, port):
