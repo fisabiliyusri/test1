f8b8e02694381bae6e71dffb353e4dc4b72b4b68
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-07-07 12:52:12 +0700

    Update ws-openssh.py

--- websocket-python/ws-openssh.py
+++ websocket-python/ws-openssh.py
@@ -2,6 +2,7 @@ import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
 LISTENING_ADDR = '0.0.0.0'
+if sys.argv[1:]:
 LISTENING_PORT = sys.argv[1]
 else:
   LISTENING_PORT = 100  
