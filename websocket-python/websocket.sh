1c60eb5b4bcc9871591bcfa77fd6e55ee49370c1
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-07-13 04:00:22 +0700

    Update websocket.sh

--- websocket-python/websocket.sh
+++ websocket-python/websocket.sh
@@ -27,10 +27,15 @@ wget -O /lib/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/${
 
 #Install Script Websocket-SSH Python
 cd
-wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/${GitUser}/test1/main/${namafolder}/ws-openssh.py && chmod +x /home/admin/ws-openssh
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/${GitUser}/test1/main/${namafolder}/ws-dropbear.py && chmod +x /home/admin/bin/ws-dropbear
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/${GitUser}/test1/main/${namafolder}/ws-stunnel.py && chmod +x /home/admin/ws-stunnel
-wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/test1/main/${namafolder}/ws-ovpn.py && chmod +x /home/admin/ws-ovpn
+wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/${GitUser}/test1/main/${namafolder}/ws-openssh && chmod +x /home/admin/ws-openssh
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/${GitUser}/test1/main/${namafolder}/ws-dropbear && chmod +x /home/admin/bin/ws-dropbear
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/${GitUser}/test1/main/${namafolder}/ws-stunnel && chmod +x /home/admin/ws-stunnel
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/test1/main/${namafolder}/ws-ovpn && chmod +x /home/admin/ws-ovpn
+#
+chmod +x /usr/local/bin/ws-openssh
+chmod +x /usr/local/bin/ws-openssh
+chmod +x /usr/local/bin/ws-openssh
+chmod +x /usr/local/bin/ws-openssh
 
 #
 cd
