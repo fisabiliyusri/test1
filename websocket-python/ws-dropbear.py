26123ebaa9b06b7f5ae06065a51b3cdd79a32ce2
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-07-13 04:06:07 +0700

    Update ws-dropbear.py

--- websocket-python/ws-dropbear.py
+++ websocket-python/ws-dropbear.py
@@ -5,15 +5,15 @@ LISTENING_ADDR = '0.0.0.0'
 if sys.argv[1:]:
   LISTENING_PORT = sys.argv[1]
 else:
-  LISTENING_PORT = 69  
+  LISTENING_PORT = 8880  
 #Pass
 PASS = ''
 
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:44'
-RESPONSE = 'HTTP/1.1 101 SL_Dropbear_YNTKTS\r\n\r\n'
+DEFAULT_HOST = '127.0.0.1:143'
+RESPONSE = 'HTTP/1.1 101 SL_CDNDropbear_YNTKTS\r\n\r\n'
 #RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 class Server(threading.Thread):
