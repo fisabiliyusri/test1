50834b8920155d005a63bf2b1f72f1c379cabd78
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-07-30 03:27:44 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -118,6 +118,7 @@ cd
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g'
 # /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 500' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 40000' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 51443' /etc/ssh/sshd_config
