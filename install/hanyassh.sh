14919f0601e9ee81ccf674b01c3acb4140a91f98
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-07-30 04:02:05 +0700

    Update hanyassh.sh

--- install/hanyassh.sh
+++ install/hanyassh.sh
@@ -275,6 +275,15 @@ chown -R www-data:www-data /home/vps/public_html
 /etc/init.d/stunnel4 restart
 /etc/init.d/vnstat restart
 /etc/init.d/squid restart
+
+#Instal DDOS Flate
+cd
+wget https://github.com/jgmdev/ddos-deflate/archive/master.zip -O ddos.zip
+unzip ddos.zip
+cd ddos-deflate-master
+./install.sh
+#
+cd
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 500
