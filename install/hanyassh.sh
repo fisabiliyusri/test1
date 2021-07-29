b42502725fedef10039eeeea714b7be16de4de10
fisabiliyusri <76268032+fisabiliyusri@users.noreply.github.com>
2021-07-30 06:25:45 +0700

    Update hanyassh.sh

--- install/hanyassh.sh
+++ install/hanyassh.sh
@@ -145,9 +145,12 @@ connect = 127.0.0.1:44
 [ssldropbear]
 accept = 777
 connect = 127.0.0.1:77
+[slws]
+accept = 443
+connect = 127.0.0.1:2087
 [stunnelws]
 accept = 2096
-connect = 2099
+connect = 127.0.0.1:2099
 [openvpn]
 accept = 992
 connect = 127.0.0.1:1194
